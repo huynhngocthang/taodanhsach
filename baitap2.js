ba02112b2e97e66387cf7c9b1603220516e070bb
huynhngocthang <huynhngocthang789@gmail.com>
2020-05-07 15:42:19 +0700

    .

---
+++ baitap2.js
@@ -0,0 +1,7 @@
+let physical=prompt,chemistry=prompt,biological=prompt;
+physical=+prompt("nhập điểm lý");
+chemistry=+prompt("nhập điểm hóa");
+biological=+prompt("nhập điểm hóa");
+average=((physical+chemistry+biological)/3);
+document.write('điểm trung bình='+average);
+document.write("<br>");
---
+++ nhietdo.js
@@ -0,0 +1,37 @@
+
+// let celsius=prompt;
+// celsius=+prompt("nhập độ c");
+// fahrenheit=((celsius/5)*9)+32;
+// document.write('độ f='+fahrenheit);
+// document.write("<br>");
+// radius=10;
+// const pi=3.14;
+// area=(radius**2)*pi;
+// perimeter=(radius*2)*pi;
+// document.write('diện tích hình tròn='+perimeter);
+// document.write('<br>');
+// document.write('chu vi hình tròn='+perimeter);
+
+function cal() {
+    let celsius;
+    celsius = +prompt('nhập độ c');
+    let fahrenheit = ((celsius / 5) * 9) + 32;
+    document.write(fahrenheit)
+    // alert(doC);
+}
+function tien() {
+let radius;
+radius=+prompt('nhập bán kính');
+const pi=3.14;
+let area=(radius**2)*pi;
+let perimeter=(radius*2)*pi;
+document.write('diện tích hình tròn='+area);
+document.write('<br>');
+document.write('chu vi hình tròn='+perimeter);
+} 
+    
+    
+
+
+
+
